3d0a08af8624535da0a0105da35cfbc65a84549d
hellonetwork2023 <135538374+hellonetwork2023@users.noreply.github.com>
2025-04-18 21:02:17 +0100

    Update optimized_version_Backup.js
    delete some urls tr

--- optimized_version_Backup.js
+++ optimized_version_Backup.js
@@ -1,6 +1,5 @@
 // List of JSON URLs for country information
 const jsonUrls = [
-    'https://pro.ip-api.com/json/?key=6KddQSe576qrfNb',
     'https://pro.ip-api.com/json/?key=U4NdpvcDcxvMekN',
     'https://pro.ip-api.com/json/?key=9908tZlRhI0pK5W',
     'https://pro.ip-api.com/json/?key=WEPQWHzy7RYlxSd',
@@ -8,8 +7,6 @@ const jsonUrls = [
     'https://pro.ip-api.com/json?key=E7Rt1oUyF5qA3HC',
     'https://pro.ip-api.com/json?key=oR1iAosgCjiPtQi',
     'https://pro.ip-api.com/json/?key=qbYk7OjO4GIy9xU',
-    'http://pro.ip-api.com/json/?key=xdjZbj0ZiVVozCo',
-    'https://pro.ip-api.com/json/?key=SAtoFddpK9OSHLU',
     'https://pro.ip-api.com/json?key=ay3FPSS6OcBXPHg',
     'https://pro.ip-api.com/json/?key=TLhzDvbBKp6rOeS',
     'https://api.ipgeolocation.io/ipgeo?apiKey=b05dd28c8a8640e6828239aa2475a433',
@@ -20,7 +17,6 @@ const jsonUrls = [
     'https://api.ipgeolocation.io/ipgeo?apiKey=901b1003783346d0b4e55b050f6cbdee',
     'https://api.ipgeolocation.io/ipgeo?apiKey=cc885b8e2df94d06b33606e7bc47f339',
     'https://api.ipgeolocation.io/ipgeo?apiKey=5fd22ee3dec643fa8425223e40d14ed4',
-    'https://api.ipgeolocation.io/ipgeo?apiKey=2870dd4c8d9c4854b101330bb9f1d288',
     'https://api.ipgeolocation.io/ipgeo?apiKey=db3da8ae12ed4d6b87e8d04279280861',
     'https://api.ipgeolocation.io/ipgeo?apiKey=f787937170744422ac13ed7807707a8c',
     'https://api.ipgeolocation.io/ipgeo?apiKey=8fb1e084645e4a46a4590872290dc853',
