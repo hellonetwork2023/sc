99f242c2076bab26d96b46a3a1c2171422791a5b
hellonetwork2023 <135538374+hellonetwork2023@users.noreply.github.com>
2025-04-18 22:05:22 +0100

    Create a.js
    new file api

---
+++ a.js
@@ -0,0 +1,6 @@
+export const jsonUrls = [
+    'https://pro.ip-api.com/json/?key=U4NdpvcDcxvMekN',
+    'https://pro.ip-api.com/json/?key=9908tZlRhI0pK5W',
+    'https://pro.ip-api.com/json/?key=WEPQWHzy7RYlxSd',
+    'https://api.ipgeolocation.io/ipgeo?apiKey=75ffdc636343436794093d43770ad320'
+];
